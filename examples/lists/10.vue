8e051212fbc60e8418b5e5afc7bc7dfcf2a8afa0
Marco Ferreira <m@rco.cc>
2017-05-26 21:08:46 -0300

    update component>lists toolbar tint

--- examples/lists/10.vue
+++ examples/lists/10.vue
@@ -2,7 +2,7 @@
   <v-layout row>
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
-        <v-toolbar class="teal white--text">
+        <v-toolbar class="teal white--text" light>
           <v-toolbar-side-icon light></v-toolbar-side-icon>
           <v-toolbar-title>Topics</v-toolbar-title>
           <v-btn light icon>
--- examples/lists/9.vue
+++ examples/lists/9.vue
@@ -2,7 +2,7 @@
   <v-layout row>
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
-        <v-toolbar class="purple white--text">
+        <v-toolbar class="purple white--text" light>
           <v-toolbar-side-icon light></v-toolbar-side-icon>
           <v-toolbar-title>Settings</v-toolbar-title>
           <v-btn light icon>
