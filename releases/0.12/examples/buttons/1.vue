9dc4dbea9c651ef02b3bd0d3d14cd177889ea7d2
John Leider <john.j.leider@gmail.com>
2017-06-04 19:16:44 -0400

    archived 0.12 in preparation for theme rework

---
+++ releases/0.12/examples/buttons/1.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container fluid class="text-xs-center">
+    <v-layout row wrap>
+      <v-flex xs12 sm6><span>Light Theme</span>
+        <v-card height="225px" class="elevation-0 white">
+          <v-card-text>
+            <div>
+              <v-btn dark flat>Normal</v-btn>
+            </div>
+            <div>
+              <v-btn dark flat class="btn--dark-flat-focused">Focused</v-btn>
+            </div>
+            <div>
+              <v-btn dark flat class="btn--dark-flat-pressed">Pressed</v-btn>
+            </div>
+            <div>
+              <v-btn dark flat disabled>Disabled</v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6><span>Dark Theme</span>
+        <v-card height="225px" class="secondary elevation-0">
+          <v-card-text>
+            <div>
+              <v-btn flat light>Normal</v-btn>
+            </div>
+            <div>
+              <v-btn flat light class="btn--light-flat-focused">Focused</v-btn>
+            </div>
+            <div>
+              <v-btn flat light class="btn--light-flat-pressed">Pressed</v-btn>
+            </div>
+            <div>
+              <v-btn flat light disabled>Disabled</v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
