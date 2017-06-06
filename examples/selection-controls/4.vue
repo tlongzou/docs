394922709d6bf05860bac94b716c042a9070f864
John Leider <jleider@fastforwardacademy.com>
2017-06-06 09:39:01 -0400

    updated examples for new scheme stuff

--- examples/selection-controls/4.vue
+++ examples/selection-controls/4.vue
@@ -4,22 +4,22 @@
       <v-container fluid>
         <v-layout row wrap>
           <v-flex xs12 sm4 md2>
-            <v-checkbox label="Primary" v-model="ex7" value="primary" primary dark hide-details></v-checkbox>
+            <v-checkbox label="Primary" v-model="ex7" value="primary" primary hide-details></v-checkbox>
           </v-flex>
           <v-flex xs12 sm4 md2>
-            <v-checkbox label="Secondary" v-model="ex7" value="secondary" secondary dark hide-details></v-checkbox>
+            <v-checkbox label="Secondary" v-model="ex7" value="secondary" secondary hide-details></v-checkbox>
           </v-flex>
           <v-flex xs12 sm4 md2>
-            <v-checkbox label="Success" v-model="ex7" value="success" success dark hide-details></v-checkbox>
+            <v-checkbox label="Success" v-model="ex7" value="success" success hide-details></v-checkbox>
           </v-flex>
           <v-flex xs12 sm4 md2>
-            <v-checkbox label="Info" v-model="ex7" value="info" info dark hide-details></v-checkbox>
+            <v-checkbox label="Info" v-model="ex7" value="info" info hide-details></v-checkbox>
           </v-flex>
           <v-flex xs12 sm4 md2>
-            <v-checkbox label="Warning" v-model="ex7" value="warning" warning dark hide-details></v-checkbox>
+            <v-checkbox label="Warning" v-model="ex7" value="warning" warning hide-details></v-checkbox>
           </v-flex>
           <v-flex xs12 sm4 md2>
-            <v-checkbox label="Error" v-model="ex7" value="error" error dark hide-details></v-checkbox>
+            <v-checkbox label="Error" v-model="ex7" value="error" error hide-details></v-checkbox>
           </v-flex>
         </v-layout>
       </v-container>
