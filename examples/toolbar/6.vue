6c03d626e98850651dbb6d854d1fbe4a52bc6937
nekosaur <albert@kaaman.nu>
2017-06-11 10:57:52 +0200

    fixed toolbar example

--- examples/toolbar/6.vue
+++ examples/toolbar/6.vue
@@ -2,6 +2,7 @@
   <v-toolbar class="secondary" dark>
       <v-toolbar-side-icon</v-toolbar-side-icon>
       <v-toolbar-title class="hidden-sm-and-down">Toolbar</v-toolbar-title>
+      <v-spacer></v-spacer>
       <v-text-field prepend-icon="search" label="Search..." hide-details single-line dark></v-text-field>
   </v-toolbar>
 </template>
