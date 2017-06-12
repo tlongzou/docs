481e0a2f3b8aeb4bc7531af90d410b0a0894b15f
John Leider <jleider@fastforwardacademy.com>
2017-06-12 13:45:19 -0400

    testing button for toolbar item

--- pages/HomeView.vue
+++ pages/HomeView.vue
@@ -4,16 +4,19 @@
       img(src="/static/v.png" height="38px")
       v-spacer
       v-toolbar-items
-        v-toolbar-item(
-          to="/vuetify/quick-start"
-          router
-        )
-          span.subheading Documentation
-        v-toolbar-item(
-          href="https://github.com/vuetifyjs/vuetify/releases"
-          target="_blank"
-        ) 
-          span.subheading Updates
+        v-btn(flat dark) Documentation
+        v-btn(flat dark) Updates
+      //- v-toolbar-items
+      //-   v-toolbar-item(
+      //-     to="/vuetify/quick-start"
+      //-     router
+      //-   )
+      //-     span.subheading Documentation
+      //-   v-toolbar-item(
+      //-     href="https://github.com/vuetifyjs/vuetify/releases"
+      //-     target="_blank"
+      //-   ) 
+      //-     span.subheading Updates
 
     section#hero.elevation-12
       v-parallax(src="/static/doc-images/vbanner.jpg" height="400")
