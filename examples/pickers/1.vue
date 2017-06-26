901b41497285cf150e3774eb14e4275eed1a284a
John Leider <john.j.leider@gmail.com>
2017-06-25 21:24:42 -0400

    revert for accidental dev code

--- examples/pickers/1.vue
+++ examples/pickers/1.vue
@@ -1,46 +1,22 @@
 <template>
-<div id="app">
-    <v-app id="inspire">
-        <v-dialog lazy v-model="outer">
-            <v-btn slot="activator">open it</v-btn>
-            <v-card>
-                <v-card-text>
-                    <form>
-                        <v-menu lazy>
-                          <v-text-field
-                                  :rules="errors"
-                                  slot="activator"
-                                  label="Start Date"
-                                  v-model="model.start_date"
-                                  prepend-icon="event"
-                                  readonly></v-text-field>
-                          <v-date-picker v-model="model.start_date" scrollable></v-date-picker>
-                      </v-menu>
-                    </form>
-                </v-card-text>
-              <v-card-row actions>
-                <v-btn primary flat @click.native="loadSampleError">Submit</v-btn>
-                </v-card-row>
-            </v-card>
-        </v-dialog>
-    </v-app>
-</div>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex md12 lg4 class="ma-3">
+        <v-date-picker v-model="picker"></v-date-picker>
+      </v-flex>
+      <v-flex md12 lg8 class="ma-3">
+        <v-date-picker v-model="picker2" landscape></v-date-picker>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   export default {
     data () {
       return {
-        errors: [],
-        outer: false,
-        model: {
-          start_date: null
-        }
-      }
-    },
-    methods: {
-      loadSampleError() {
-        this.errors = ['This could be a server side error'];
+        picker: null,
+        picker2: null
       }
     }
   }
