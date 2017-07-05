bedfc194dd27c9d759b22e079e899fecd381d29e
nekosaur <albert@kaaman.nu>
2017-07-05 23:53:06 +0200

    updated date picker example

--- examples/pickers/13.vue
+++ examples/pickers/13.vue
@@ -1,13 +1,19 @@
 <template>
   <v-container fluid>
-    <v-layout justify-center>
+    <v-layout justify-space-between>
       <v-date-picker
-        :months="months"
-        :days="days"
         first-day-of-week="星期日"
-        :short-days="(d) => { return d.slice(-1) }"
-        :title-date-format="({ dayName, monthName, day }) => { return `${monthName}${day}日, ${dayName}` }"
-        :header-date-format="({ monthName, year }) => { return `${year}年 ${monthName}` }"
+        locale="zh-cn"
+        v-model="picker"
+      ></v-date-picker>
+      <v-date-picker
+        first-day-of-week="måndag"
+        locale="sv-se"
+        v-model="picker"
+      ></v-date-picker>
+      <v-date-picker
+        first-day-of-week="lunes"
+        locale="es-sp"
         v-model="picker"
       ></v-date-picker>
     </v-layout>
@@ -18,9 +24,7 @@
   export default {
     data() {
       return {
-        picker: null,
-        months: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'].map(x => x + '月'),
-        days: ['日', '一', '二', '三', '四', '五', '六'].map(x => '星期' + x),
+        picker: null
       }
     }
   }
