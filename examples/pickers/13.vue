6cc167d79696a23f001bc04ba361fc25a318650b
Albert Kaaman <albert@kaaman.nu>
2017-07-06 09:17:02 +0200

    Updated example

--- examples/pickers/13.vue
+++ examples/pickers/13.vue
@@ -2,17 +2,17 @@
   <v-container fluid>
     <v-layout justify-space-between>
       <v-date-picker
-        first-day-of-week="星期日"
+        :first-day-of-week="0"
         locale="zh-cn"
         v-model="picker"
       ></v-date-picker>
       <v-date-picker
-        first-day-of-week="måndag"
+        :first-day-of-week="1"
         locale="sv-se"
         v-model="picker"
       ></v-date-picker>
       <v-date-picker
-        first-day-of-week="lunes"
+        :first-day-of-week="1"
         locale="es-sp"
         v-model="picker"
       ></v-date-picker>
