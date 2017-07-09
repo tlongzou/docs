4ec80db98fb0955d3c7dd2c0384e2389e7ad8304
Jürg Rast <juergr@gmail.com>
2017-07-09 20:52:55 +0200

    Fixed github link to the list component

--- pages/ListsView.vue
+++ pages/ListsView.vue
@@ -11,7 +11,7 @@
       return {
         doc: {
           title: 'List',
-          component: 'list',
+          component: 'lists',
           edit: 'ListsView',
           desc: 'The <code>v-list</code> component is used to display information. It can contain an avatar, content, actions, subheaders and much more. Lists can contain children and are used in the sidebar.',
           examples: [
