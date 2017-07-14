40f8ce0b60792743eaaa8f2673b8bce529fc9535
John Leider <jleider@fastforwardacademy.com>
2017-07-14 12:20:54 -0400

    enhancement for #101

--- pages/FaqView.vue
+++ pages/FaqView.vue
@@ -4,7 +4,7 @@
       v-flex(xs12 sm8 md12)
         section-def
           dt(slot="title") Frequently asked questions
-          dd(slot="desc") Stuck on a particular problem? Check some of these common gotchas before creating a ticket. If you still cannot find what you are looking for, submit an <a href="https://github.com/vuetifyjs/vuetify/issues/new" target="_blank" rel="noopener">issue</a> on github.
+          dd(slot="desc") Stuck on a particular problem? Check some of these common gotchas before creating a ticket. If you still cannot find what you are looking for, submit an <a href="https://github.com/vuetifyjs/vuetify/issues/new" target="_blank" rel="noopener">issue</a> on github or ask the community in <a href="https://gitter.im/vuetifyjs/Lobby" target="_blank" rel="noopener">gitter</a>.
       ad
     section
       v-expansion-panel
