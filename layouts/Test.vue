55dff48ec08489ff5e343eb8a54d361244bcc549
John Leider <jleider@fastforwardacademy.com>
2017-07-18 16:10:13 -0400

    updated layout

--- layouts/Test.vue
+++ layouts/Test.vue
@@ -1,15 +1,24 @@
 <template lang="pug">
   v-app
-    v-navigation-drawer
+    v-navigation-drawer(v-model="drawer")
     main
       v-toolbar
+        v-toolbar-side-icon(@click="drawer = !drawer")
       v-content
-        v-container
-          v-layout
+        v-container(fluid fill-height)
+          v-layout(align-center justify-center)
             v-flex(xs12)
-              v-card(height="200vh")
+              v-card(height="200px")
                 v-toolbar(flat)
                 v-card-text Hello
         v-divider
         v-footer
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: true
+    })
+  }
+</script>
